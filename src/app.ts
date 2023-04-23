c608ebd6a7978ff72b361680b2d432b0557ee0de
amoutai <amin.moutai@gmail.com>
2023-04-23 16:52:54 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
+import { Server } from 'socket.io';
 
 const app = express();
+const io = new Server();
 
 app.get('/', (req: Request, res: Response) => {
-  return res.send('ok2');
+	return res.send('ok2');
 });
 
 export default app;
